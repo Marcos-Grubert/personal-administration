ef1d4ee2aee7fb2ef8914dcebad8655b3b06e3a2
marcos <mgmarcosgrubert@gmail.com>
2026-03-30 20:04:43 -0300

    chore: Added initial files of project and your dependencies

---
+++ src/main/java/br/com/eventhorizon/personaladminsitration/PersonaladminsitrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.eventhorizon.personaladminsitration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaladminsitrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaladminsitrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eventhorizon/personaladminsitration/PersonaladminsitrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eventhorizon.personaladminsitration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaladminsitrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
