c8c827d00991ba0364e3a6fd52b4ac69842309de
marcos <mgmarcosgrubert@gmail.com>
2026-04-21 20:54:15 -0300

    feat: implementado consistência para emails duplicados e persistências de usuários no banco de dados.

--- src/main/java/br/com/eventhorizon/personaladminsitration/register/users/UserService.java
+++ src/main/java/br/com/eventhorizon/personaladminsitration/register/users/UserService.java
@@ -1,4 +1,32 @@
 package br.com.eventhorizon.personaladminsitration.register.users;
 
+import br.com.eventhorizon.personaladminsitration.register.users.dto.UserCreateDto;
+import br.com.eventhorizon.personaladminsitration.register.users.dto.UserResponseDto;
+import br.com.eventhorizon.personaladminsitration.register.users.exeption.EmailAlreadyInUseException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserEntity findByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    }
+
+    public UserResponseDto create(UserCreateDto userCreateDto) {
+        if(userRepository.findByEmail(userCreateDto.email()).isPresent()) {
+            throw new EmailAlreadyInUseException("E-mail informado já está em uso.");
+        }
+        UserEntity user = UserMapper.toEntity(userCreateDto);
+        user.setPasswordHash(new BCryptPasswordEncoder().encode(userCreateDto.password()));
+        user.setActive(true);
+        user.setCreatedBy(1L);
+        UserEntity savedUser = userRepository.save(user);
+
+        return UserMapper.toResponse(savedUser);
+    }
 }
