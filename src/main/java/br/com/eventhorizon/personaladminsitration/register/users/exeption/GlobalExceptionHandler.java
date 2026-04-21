e61eecfa76972680c4820631642cc122530da35f
marcos <mgmarcosgrubert@gmail.com>
2026-04-21 20:49:47 -0300

    feat: implementa tratativas para excessões onde o email ja existe.

--- src/main/java/br/com/eventhorizon/personaladminsitration/register/users/exeption/GlobalExceptionHandler.java
+++ src/main/java/br/com/eventhorizon/personaladminsitration/register/users/exeption/GlobalExceptionHandler.java
@@ -1,4 +1,22 @@
 package br.com.eventhorizon.personaladminsitration.register.users.exeption;
 
+import br.com.eventhorizon.personaladminsitration.register.users.dto.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(EmailAlreadyInUseException.class)
+    public ResponseEntity<ErrorResponseDto> handleEmailAlreadyInUseException(EmailAlreadyInUseException ex){
+        ErrorResponseDto error = new ErrorResponseDto(
+                HttpStatus.CONFLICT.value(),
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
 }
