4f1ed3ce320530b32d3158582f9d2e8451e59bbb
marcos <mgmarcosgrubert@gmail.com>
2026-04-21 20:55:56 -0300

    feat: criado porta para criar usuários no banco de dados.

--- src/main/java/br/com/eventhorizon/personaladminsitration/register/users/UserController.java
+++ src/main/java/br/com/eventhorizon/personaladminsitration/register/users/UserController.java
@@ -1,4 +1,23 @@
 package br.com.eventhorizon.personaladminsitration.register.users;
 
+import br.com.eventhorizon.personaladminsitration.register.users.dto.UserCreateDto;
+import br.com.eventhorizon.personaladminsitration.register.users.dto.UserResponseDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/register/users")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/createUser")
+    public UserResponseDto createUser(@RequestBody UserCreateDto userCreateDto) {
+        return userService.create(userCreateDto);
+    }
 }
