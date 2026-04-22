e418ca5cb0ae94b2400100951d8eb13886df1217
marcos <mgmarcosgrubert@gmail.com>
2026-04-21 21:04:04 -0300

    feat: implementa UserMapper para conversão entre DTO e Entidade.

--- src/main/java/br/com/eventhorizon/personaladminsitration/register/users/UserMapper.java
+++ src/main/java/br/com/eventhorizon/personaladminsitration/register/users/UserMapper.java
@@ -1,4 +1,22 @@
 package br.com.eventhorizon.personaladminsitration.register.users;
 
+import br.com.eventhorizon.personaladminsitration.register.users.dto.UserCreateDto;
+import br.com.eventhorizon.personaladminsitration.register.users.dto.UserResponseDto;
+
 public class UserMapper {
+    public static UserEntity toEntity(UserCreateDto userCreateDto){
+        UserEntity user = new UserEntity();
+        user.setName(userCreateDto.name());
+        user.setEmail(userCreateDto.email());
+        return user;
+    }
+
+    public static UserResponseDto toResponse(UserEntity user) {
+        return new UserResponseDto(
+                user.getUserId(),
+                user.getName(),
+                user.getEmail(),
+                user.isActive()
+        );
+    }
 }
