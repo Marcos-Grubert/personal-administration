569952d0380d59fd13ac7e7c829a1b0c3438b854
marcos <mgmarcosgrubert@gmail.com>
2026-04-21 21:17:02 -0300

    feat: implementa excessão para emails em uso no banco.

--- src/main/java/br/com/eventhorizon/personaladminsitration/register/users/exeption/EmailAlreadyInUseException.java
+++ src/main/java/br/com/eventhorizon/personaladminsitration/register/users/exeption/EmailAlreadyInUseException.java
@@ -1,4 +1,7 @@
 package br.com.eventhorizon.personaladminsitration.register.users.exeption;
 
 public class EmailAlreadyInUseException extends RuntimeException{
+    public EmailAlreadyInUseException(String message) {
+        super(message);
+    }
 }
