09135d8da80042e1160f4552e464b476e622853b
marcos-grubert <mgmarcosgrubert@gmail.com>
2026-04-26 12:32:31 -0300

    feat: adicionado mapper e encoder ao serviço

--- src/main/java/br/com/eventhorizon/personaladminsitration/register/users/UserService.java
+++ src/main/java/br/com/eventhorizon/personaladminsitration/register/users/UserService.java
@@ -2,15 +2,19 @@ package br.com.eventhorizon.personaladminsitration.register.users;
 
 import br.com.eventhorizon.personaladminsitration.register.users.dto.UserCreateDto;
 import br.com.eventhorizon.personaladminsitration.register.users.dto.UserResponseDto;
-import br.com.eventhorizon.personaladminsitration.register.users.exeption.EmailAlreadyInUseException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import br.com.eventhorizon.personaladminsitration.register.users.exception.EmailAlreadyInUseException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
-    public UserService(UserRepository userRepository) {
+    private final PasswordEncoder passwordEncoder;
+    private final UserMapper userMapper;
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.userMapper = userMapper;
     }
 
     public UserEntity findByEmail(String email) {
@@ -21,12 +25,12 @@ public class UserService {
         if(userRepository.findByEmail(userCreateDto.email()).isPresent()) {
             throw new EmailAlreadyInUseException("E-mail informado já está em uso.");
         }
-        UserEntity user = UserMapper.toEntity(userCreateDto);
-        user.setPasswordHash(new BCryptPasswordEncoder().encode(userCreateDto.password()));
+        UserEntity user = userMapper.toEntity(userCreateDto);
+        user.setPasswordHash(passwordEncoder.encode(userCreateDto.password()));
         user.setActive(true);
         user.setCreatedBy(1L);
         UserEntity savedUser = userRepository.save(user);
 
-        return UserMapper.toResponse(savedUser);
+        return userMapper.toResponse(savedUser);
     }
 }
