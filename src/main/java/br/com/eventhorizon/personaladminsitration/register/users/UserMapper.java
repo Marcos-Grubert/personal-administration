9702570159c24fa2251af2fd3ac8eaa1765e5be0
marcos-grubert <mgmarcosgrubert@gmail.com>
2026-04-26 12:29:44 -0300

    feat: transformado a classe em um componente para o spring gerenciar

--- src/main/java/br/com/eventhorizon/personaladminsitration/register/users/UserMapper.java
+++ src/main/java/br/com/eventhorizon/personaladminsitration/register/users/UserMapper.java
@@ -2,16 +2,18 @@ package br.com.eventhorizon.personaladminsitration.register.users;
 
 import br.com.eventhorizon.personaladminsitration.register.users.dto.UserCreateDto;
 import br.com.eventhorizon.personaladminsitration.register.users.dto.UserResponseDto;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserMapper {
-    public static UserEntity toEntity(UserCreateDto userCreateDto){
+    public UserEntity toEntity(UserCreateDto userCreateDto){
         UserEntity user = new UserEntity();
         user.setName(userCreateDto.name());
         user.setEmail(userCreateDto.email());
         return user;
     }
 
-    public static UserResponseDto toResponse(UserEntity user) {
+    public UserResponseDto toResponse(UserEntity user) {
         return new UserResponseDto(
                 user.getUserId(),
                 user.getName(),
